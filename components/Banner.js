2ae03d1ae2c92efeed6cd3c5fff9eaf464eeb399
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-07-06 12:14:11 -0300

    LA CARDDDDDD

--- components/Banner.js
+++ components/Banner.js
@@ -1,22 +1,27 @@
 import { StyleSheet, Text, View,Image } from 'react-native';
+import { color } from 'react-native-elements/dist/helpers';
 export default function Banner() {
   return (
     <View>
       <Text></Text>
         <Image
          style={styles.logo}
-         source={require('../central_perk_logo.jpeg')}
+         source={require('../central_perk_logo.PNG')}
          />
+         <Text style={styles.textoo}>Proba Nuestro Cafe</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   textoo: {
-    fontSize:"20px"
+    fontSize:"30px",
+    color:"white",
+    display:"flex",
+    alignSelf:"center"
   },
   logo: {
     width: 400,
-    height: 150
+    height: 150,
   },
 });
--- components/Card.js
+++ components/Card.js
@@ -1,21 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import { Card, ListItem, Button, Icon } from 'react-native-elements'
+import { ImageBackground } from 'react-native-web';
 
 export default function Cards() {
   return (
-    <View >
-      <Text style={styles.texto}>Try some of our most popular flavors!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.view}>
+      <Card title="CARD WITH DIVIDER" style={styles.card}>
+          <Image
+            style={styles.image}
+            source={require('../coffee1.jpeg')}
+          />
+          <div style={styles.division}>
+          <Text style={styles.texto}>Try some of our most popular flavors!</Text>
+          </div>
+        </Card>
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
+  image: {
+    width: 400,
+    height: 400,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  card: {
+    backgroundcolor: "transparent",
+     borderwidth: "0px"
+  },
+  division: {
+    backgroundColor:'green',
+    borderBottomRightRadius: 30,
+    borderBottomLeftRadius: 30,
+  },
   texto:{
-    color:"white",
-    marginTop: "30px"
-    
-    //Font-size o algo asi
+    color:'white', 
+    display: 'flex', 
+    justifyContent:"center", 
+    alignItems:"center", 
+    margin:'1rem', 
+    fontSize:'1.2rem', 
+    fontWeight: 'bold',
+  },view:{
+    backgroundColor:"transparent"
   }
 });
